4e079e761f99e3e1bae1c23bd1ae4c45dca1957a
akash <akash300383@gmail.com>
2021-08-18 14:35:14 +0530

    done

---
+++ Node JS Programs/Node JS mongoose module/updateQuery.js
@@ -0,0 +1,33 @@
+let obj = require("mongoose");
+let url = "mongodb://localhost:27017/mern"; // load and connect database 
+let options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+obj.connect(url,options).then(res=>console.log("connected")).catch(error=>console.log(error));
+let db = obj.connection;        // get the connection reference. 
+
+// life cycle function 
+db.once("open",()=> {
+    // create the Schema 
+    var customerSchema = obj.Schema({
+        _id:Number,
+        name:String,
+        age:Number
+    });
+// using schema create model 
+//var customerModel = obj.model("Customer",customerSchema); it create collection in lower case with s post fix. 
+var customerModel = obj.model("",customerSchema,"Customer");
+
+    customerModel.updateOne({_id:1},{$set:{age:29}},(err,result)=> {
+        if(!err){
+            if(result.nModified>0){
+                    console.log("Updated")
+            }else {
+                    console.log("didn't update")
+            }
+        }
+        obj.disconnect();
+    })
+
+})
