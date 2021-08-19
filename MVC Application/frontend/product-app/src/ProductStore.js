1a47c7f7ac4623e0d11c7749037cbc82b3a00d38
akash <akash300383@gmail.com>
2021-08-19 16:11:11 +0530

    done

--- MVC Application/frontend/product-app/src/ProductStore.js
+++ MVC Application/frontend/product-app/src/ProductStore.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default class ProductStore extends Component {
     constructor(props){
         super(props);
-        this.state = {_id:0,pname:"",price:0.0}
+        this.state = {_id:0,pname:"",price:0.0,msg:""}
     }
     handleChange=(event)=> {
         this.setState({[event.target.name]:event.target.value});
@@ -14,7 +14,13 @@ export default class ProductStore extends Component {
         //console.log(product);
         //1st parameter is url and 2nd parameter json data. 
         axios.post("http://localhost:9090/api/product/storeProductDetails",product).
-        then(result=>console.log(result)).catch(error=>console.log(error));
+        then(result=>{
+            if(result.data.result == undefined){
+                    this.setState({"msg":"Record inserted successfully"})
+            }else {
+                this.setState({"msg":"record didn't store"})
+            }
+        }).catch(error=>console.log(error));
     }
     render() {
         return (
@@ -29,6 +35,8 @@ export default class ProductStore extends Component {
                 <input type="submit" value="Store"/>
                 <input type="reset" value="reset"/>    
             </form>     
+            <br/>
+            <span style={{'color':'red'}}>{this.state.msg}</span>
             </div>
         )
     }
