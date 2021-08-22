a7ca66044542b3f174670167e80bdd0e28de7375
akash <akash300383@gmail.com>
2021-08-22 16:36:47 +0530

    done

---
+++ Node JS Programs/Socket Programming/web socket/server.js
@@ -0,0 +1,21 @@
+// load the express module 
+let express = require("express");
+// create the reference of express 
+let app = express();
+//Now we have to create reference of web socket with the 
+// help express reference. 
+let ws = require("express-ws")(app);
+// http://localhost:9090 
+app.get("/",(req,res)=> {
+    // __dirname : it provide current directory path 
+    res.sendFile(__dirname+"/index.html");
+})
+app.ws("/",(socket,req)=> {
+        console.log("Web Socket connection done successfully...");
+        // To recieve the message from client 
+        socket.on("message",(data)=> {
+            console.log(data);
+        })
+})
+
+app.listen(9090,()=>console.log("Server running on port number 9090"));
