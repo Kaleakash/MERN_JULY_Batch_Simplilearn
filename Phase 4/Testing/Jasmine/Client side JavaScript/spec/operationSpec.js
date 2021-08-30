50122596470f2998b8b4a8ceee26f30b0e102960
akash <akash300383@gmail.com>
2021-08-30 17:54:25 +0530

    done

--- Phase 4/Testing/Jasmine/Client side JavaScript/spec/operationSpec.js
+++ Phase 4/Testing/Jasmine/Client side JavaScript/spec/operationSpec.js
@@ -12,4 +12,20 @@ describe("Operation Testing",()=> {
         var result = add(10,20);        // calling JS function 
         expect(30).toBe(result);
     })
+
+    it("Substraction function testing",()=> {
+        var result = sub(20,10);
+        expect(10).toBe(result);
+    })
+})
+
+describe("Employee Testing",()=> {
+    it("Calulate Salary ",()=> {
+            var managerSalary = calSalary(100,"Raj",45000,"Manager");
+            var programmerSalary = calSalary(101,"Ramesh",25000,"Programmer");
+            var testerSalary = calSalary(102,"Raju",15000,"Tester");
+            expect(50000).toBe(managerSalary);
+            expect(28000).toBe(programmerSalary);
+            expect(15000).toBe(testerSalary);                
+    })
 })
--- Phase 4/Testing/Jasmine/Client side JavaScript/src/operation.js
+++ Phase 4/Testing/Jasmine/Client side JavaScript/src/operation.js
@@ -1,5 +1,27 @@
 function add(a,b){
     var sum = a+b;
-    return 0;
+    return sum;
+}
+function sub(a,b){
+    var sub1 = a-b;
+    return sub1;
+}
+function mul(a,b){
+    var mul1 = a*b;
+    return mul1;
+}
+function div(a,b){
+    var div1 = a/b;
+    return div1;
+}
+
+function calSalary(id,name,salary,desg){
+    if(desg=="Manager"){
+        return salary+5000;
+    }else if(desg=="Programmer"){
+        return salary+3000;
+    }else {
+        return salary;
+    }
 }
 
